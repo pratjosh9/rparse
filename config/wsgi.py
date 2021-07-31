aae21cd82c3d52c908ea7701a9e405234b4fb88f
Pratik Joshi <pratikjoshi.work@gmail.com>
2021-07-31 21:15:19 +0530

    update wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+settings_module = os.environ.get("DJANGO_SETTINGS_MODULE")
+
+if settings_module is None:
+    settings_module = "config.local_settings"
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
